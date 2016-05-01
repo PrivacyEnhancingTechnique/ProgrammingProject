af2d4f69de3dd3a92a72afac3d058040482851c5
sreyd <sreymoch@yahoo.com>
2016-05-01 18:38:06 -0500

    Update EncryptApp.java

--- EncryptApp.java
+++ EncryptApp.java
@@ -1,3 +1,5 @@
+package cs898ABProject;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,16 +9,21 @@ import com.dropbox.core.DbxEntry;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.DbxWriteMode;
+import static cs898ABProject.Authenticate.download;
+import static cs898ABProject.Authenticate.getFileDirectory;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import net.miginfocom.swing.MigLayout;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.crypto.NoSuchPaddingException;
 import javax.swing.*;
 
@@ -38,10 +45,10 @@ public class EncryptApp extends JPanel
 	 JLabel passWord = new JLabel("Password");
 	 JLabel dropBox = new JLabel("Dropbox Account?");
 	 JLabel appKeyDropBox = new JLabel("App Key");
-	 JLabel dropboxDirLabel = new JLabel("Upload Location");
-	 JTextArea dropboxDirText = new JTextArea("");
 	 JTextArea appKeyDropBoxText = new JTextArea("");
 	 JButton uploadButton = new JButton ("Upload");
+         JLabel dropboxDirLabel = new JLabel("Upload Location");
+	 JTextArea dropboxDirText = new JTextArea("");
 	 SpinnerModel model = new SpinnerNumberModel(1, 0, 20, 1);
 	 JSpinner chunkSize = new JSpinner(model);
 	 JRadioButton yesGoogle = new JRadioButton("Yes");
@@ -74,8 +81,9 @@ public class EncryptApp extends JPanel
 	     passWordTextArea.setBackground(new Color(250,250,250));
 	     appKeyDropBoxText.setPreferredSize(new Dimension(200, 20));
 	     appKeyDropBoxText.setBackground(new Color(250,250,250));
-	     dropboxDirText.setPreferredSize(new Dimension(200, 20)); 
+             dropboxDirText.setPreferredSize(new Dimension(200, 20)); 
 	     dropboxDirText.setBackground(new Color(250,250,250));
+	   
 	     
 	     //value chooser
 	     chunkSize.setBounds(0, 0, 10, 10);
@@ -128,7 +136,7 @@ public class EncryptApp extends JPanel
 	        encryptPanel.add(appKeyDropBox);
 	        encryptPanel.add(appKeyDropBoxText);
 	        encryptPanel.add(spacer=  new JLabel(" "), "span, grow");
-	        encryptPanel.add(dropboxDirLabel);
+                encryptPanel.add(dropboxDirLabel);
 	        encryptPanel.add(dropboxDirText);
 	        encryptPanel.add(spacer=  new JLabel(" "), "span, grow");
 	        encryptPanel.add(spacer=  new JLabel(" "), "span, grow");
@@ -154,18 +162,9 @@ public class EncryptApp extends JPanel
 	            if (returnVal == JFileChooser.APPROVE_OPTION) {
 	                File file = fc.getSelectedFile();
 	                //This is where a real application would open the file.
-	                System.out.println("Opening: "+fc.getCurrentDirectory()+ "\\" + file.getName() );
+	                System.out.println("Opening: "+fc.getCurrentDirectory() + file.getName() );
 	                textArea.setText(fc.getCurrentDirectory() + "\\" + file.getName());
 	                setFileName(fc.getCurrentDirectory() + "\\" +file.getName());
-	                
-	                /*try {
-						EncryptDecrypt encrpt = new EncryptDecrypt();
-						encrpt.encryptFile("me");
-					} catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}*/
-	                
 	            } else {
 	            	System.out.println("Open command cancelled by user.");
 	            }
@@ -181,32 +180,64 @@ public class EncryptApp extends JPanel
                         Logger.getLogger(MainInterface.class.getName()).log(Level.SEVERE, null, ex);
                     } catch (DbxException ex) {
                         Logger.getLogger(MainInterface.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InvalidKeyException ex) {
+                        Logger.getLogger(MainInterface.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
 	    
 		}
 
-   public void uploadToDropbox() throws IOException, DbxException{
+   public void uploadToDropbox() throws IOException, DbxException, InvalidKeyException{
+       EncryptDecrypt encryptObj; 
         DbxClient client = authenticateDropBox();
         
-        //Upload file
-        File inputFile = new File(openButton.getText());
-        FileInputStream inputStream = new FileInputStream(inputFile);
+        //Split file
+        ArrayList<String> fileList = splitFile(fileName, (Integer)model.getValue());
+        
+        //Encrypt
         try {
-            DbxEntry.File uploadedFile = client.uploadFile("/Project_Proposal.docx",
-                DbxWriteMode.add(), inputFile.length(), inputStream);
-            System.out.println("Uploaded: " + uploadedFile.toString());
-        } finally {
-            inputStream.close();
+            encryptObj = new EncryptDecrypt();
+            for (int index = 0; index < fileList.size(); index++) {
+                encryptObj.encryptFile(fileList.get(index));
+            }
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+        }
+        
+        //Upload file
+        String directory = getFileDirectory(dropboxDirText.getText());
+
+        //Upload each sub file
+        for (String file : fileList) {
+            File inputFile = new File(file);
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            System.out.print(inputFile.getName());
+            try {
+                DbxEntry.File uploadedFile = client.uploadFile(directory + inputFile.getName(),
+                    DbxWriteMode.add(), inputFile.length(), inputStream);
+                System.out.println("Uploaded: " + uploadedFile.toString());
+            } finally {
+                inputStream.close();
+            }
         }
    }
+   
+   public static String getFileDirectory(String directory) {
+        //Add appropriate slash if the directory does not end with one
+        if (directory != "/"){
+            if (directory.contains("/") && !directory.endsWith("/")) {
+                directory += "//";
+            }
+        }
+        return directory;
+    }
    
    public DbxClient authenticateDropBox() throws DbxException {
        DbxRequestConfig config = new DbxRequestConfig(
             "CS898AB Project", Locale.getDefault().toString());
         //bypass confirm method
         String accessToken = appKeyDropBoxText.getText();
-                //"FFrQLoDw4SAAAAAAAAAAJOMwQojXyvwIxZnFwMtWu9nrSyn_8kO7AgdX14_dniwN";
 
         DbxClient client = new DbxClient(config, accessToken);
 
@@ -214,15 +245,40 @@ public class EncryptApp extends JPanel
         return client;
    }
    
-   public void downloadFromDropBox() throws IOException, DbxException {
-       DbxClient client = authenticateDropBox();
-       FileOutputStream outputStream = new FileOutputStream("/Users/sreymoct/class/cs898AB/Copy_Project_Proposal.docx");
-        try {
-            DbxEntry.File downloadedFile = client.getFile("/Project_Proposal.docx", null,
-                outputStream);
-            System.out.println("Metadata: " + downloadedFile.toString());
-        } finally {
-            outputStream.close();
+   public static ArrayList<String> splitFile(String file, int fileCount) 
+            throws FileNotFoundException, IOException {
+        ArrayList<String> subFileList = new ArrayList<String>(fileCount);
+        
+        //Open user provided file
+        BufferedInputStream inputFile = new BufferedInputStream(
+                new FileInputStream(file));
+
+        // get the file length
+        File userFile = new File(file);
+        long fileLength = userFile.length();
+        
+        // Get file name
+        String fileName = userFile.getName();
+        
+        //Get the chunk size for each sub files
+        long fileChunkSize = fileLength / fileCount;
+        
+        //For each subFile
+        for (int fileNumber = 0; fileNumber < fileCount; fileNumber++) {
+            String subFileName = "file_" + fileNumber + "_" + fileName;
+            BufferedOutputStream outputFile = new BufferedOutputStream(
+                    new FileOutputStream(subFileName));
+
+            //Write a number of bytes to a sub file
+            for (int byteCount = 0; byteCount < fileChunkSize; byteCount++) {
+                // write one byte from input file to output file
+                outputFile.write(inputFile.read());
+            }
+
+            subFileList.add(subFileName);
+            outputFile.close();
         }
-   }
+
+        return subFileList;
+    }
 }
